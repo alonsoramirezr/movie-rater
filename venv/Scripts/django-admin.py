a16a8c23f9748fe708b7901cfe2a033b3d6f0b2c
alonsoramirezr <a.ramirez.160595@gmail.com>
2020-05-22 11:28:35 -0400

    Push del projecto backend construido para el projecto movie rater

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\arami\onedrive\escritorio\desarrollo\python\tutorial\movieraterapi\venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
